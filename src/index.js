ba07c54fc40000768c4a8f198269d5284ef15956
avgre <avtar.g10@gmail.com>
2021-06-01 18:31:11 -0400

    Config Router + Redux -add store/reducer from old hue app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import createStore from './store.js';
 import reportWebVitals from './reportWebVitals';
-
+const { store } = createStore();
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/store.js
@@ -0,0 +1,56 @@
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const initialState = {
+  hubAddress: undefined,
+  hubUsername: undefined,
+  lights: {},
+  selectedLight: '',
+  connected: false,
+  gameName: '',
+  globalOptions: {
+    players: 2,
+    sound: 'on',
+  },
+  games: [],
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_GAMES':
+      return { ...state, games: action.payload };
+    case 'INITIALIZE_GAME':
+      return { ...state, gameName: action.payload };
+    case 'CHANGE_COLOR':
+      return { ...state, currentColor: action.payload };
+    case 'SET_BRIDGE_IP':
+      return { ...state, hubAddress: action.payload };
+    case 'SET_USER':
+      return { ...state, hubUsername: action.payload };
+    case 'SET_LIGHTS':
+      return { ...state, lights: action.payload };
+    case 'SET_PLAYLIGHT':
+      return { ...state, selectedLight: action.payload };
+    case 'SET_CONNECTED':
+      return { ...state, connected: action.payload };
+    default:
+      return state;
+  }
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
