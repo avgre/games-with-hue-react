3a03a1f04b687209e8b9b5b73cb0f04ea69bf956
avgre <avtar.g10@gmail.com>
2021-06-01 18:33:35 -0400

    Configure App Global Styles with Styled Components

---
+++ src/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', sans-serif;
+    background: #322290;
+    color: white;
+  }
+`;
+
+export default GlobalStyle;
